c2fad5b7a368cdf579d618dfcc26db803cc80270
guzel <guzel.kh23@gmail.ru>
2023-07-04 11:48:57 +0300

    class Channel fixed according recomendations

--- src/channel.py
+++ src/channel.py
@@ -13,9 +13,8 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel_id = self.channel_id
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        print(channel)
+        print(self.channel)
